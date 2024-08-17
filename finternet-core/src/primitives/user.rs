db70a5dc5ebe67c422a87ab7831362063c304125
abishekk92 <602823+abishekk92@users.noreply.github.com>
2024-08-17 15:24:23 +0530

    finternet-core: simplify asset_ledger

--- finternet-core/src/primitives/user.rs
+++ finternet-core/src/primitives/user.rs
@@ -22,10 +22,6 @@ pub struct User {
     /// User Asset Config
     pub asset_config: HashMap<FinternetUID, HashMap<String, String>>, // Asset specific config like
     // whitelisted senders, spend limits etc
-    /// Incoming Asset Ledger
-    pub incoming_asset_ledger: HashMap<FinternetUID, HashMap<FinternetUID, u64>>, // Incoming Asset Ledger for the user (Asset ID, Sender, Units)
-    /// Outgoing Asset Ledger
-    pub outgoing_asset_ledger: HashMap<FinternetUID, HashMap<FinternetUID, u64>>, // Incoming Asset Ledger for the user (Asset ID, Sender, Units)
     /// User Asset Ledger
     pub asset_ledger: HashMap<FinternetUID, u64>, // Asset Ledger for the user (Asset ID, Units)
 }
