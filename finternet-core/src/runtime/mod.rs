6d8cc60509ffa32fbeb6f0bacf4e6d0262de71a1
abishekk92 <602823+abishekk92@users.noreply.github.com>
2024-08-22 13:59:11 +0530

    Add saver and executor traits

--- finternet-core/src/runtime/mod.rs
+++ finternet-core/src/runtime/mod.rs
@@ -3,7 +3,16 @@ use crate::primitives::core::FinternetUID;
 use serde::de::Deserializer;
 
 /// Trait for loading the data into memory
-// // __NOTE__: Firm up the return type, impl Deserializer is lazy
 pub trait Loader {
     fn load(&self, id: FinternetUID) -> Result<impl Deserializer, String>;
+}
+
+/// Trait for saving the data from memory
+pub trait Saver<'a> {
+    fn save(&self, id: FinternetUID, data: impl Deserializer<'a>) -> Result<(), String>;
+}
+
+/// Trait to execute the transaction
+pub trait Executor {
+    fn execute(&self, transaction_bytes: Vec<u8>) -> Result<(), String>;
 }
